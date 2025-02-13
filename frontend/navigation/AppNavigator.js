d54cccf772f9674390ae41d096ef444e80d1eced
dding <myungjinkang8@gmail.com>
2025-02-14 02:21:27 +0900

    하단탭이슈 수정 + 보관함 스크린연결 초기

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -1,3 +1,5 @@
+//AppNavigator.js
+
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
--- frontend/navigation/MedicineNavigator.js
+++ frontend/navigation/MedicineNavigator.js
@@ -1,7 +1,10 @@
+//MedicineNavigator.js
+
+
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import MedicineScreen from "../screens/MedicineScreens/MedicineScreen";
-import MedicineDetailScreen from "../screens/MedicineScreens/MedicineDetailScreen";
+
 
 const Stack = createStackNavigator();
 
@@ -9,7 +12,8 @@ export default function MedicineNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="MedicineMain" component={MedicineScreen} />
-      <Stack.Screen name="MedicineDetail" component={MedicineDetailScreen} />
+      
+      
     </Stack.Navigator>
   );
 }
