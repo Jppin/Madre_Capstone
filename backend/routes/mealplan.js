95b286592fc4879577128b3668506052f086e64d
유현서 <pipi2938@naver.com>
2025-05-05 23:02:46 +0900

    식단 기능에 성분 비율(양) 반영

--- backend/routes/mealplan.js
+++ backend/routes/mealplan.js
@@ -1,9 +1,14 @@
 // /routes/mealplan.js
 import express from "express";
 import { generateMealPlan } from "../controllers/mealplanController.js";
+import { authenticateUser } from "../middleware/auth.js";
+import { getEnergyAndMacroInfo } from "../controllers/mealplanController.js";
+
 
 const router = express.Router();
 
 router.post("/generate", generateMealPlan);
+router.get("/energy-macro", authenticateUser, getEnergyAndMacroInfo);
+
 
 export default router;
